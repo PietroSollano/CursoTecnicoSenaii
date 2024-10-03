3af4e2aca5b67f0133774d8e783f5fa9aac83895
Pietro <pietroutra489@gmail.com>
2024-10-03 14:10:41 -0300

    atualizando arquivos

---
+++ Logica de Programaçao/caixadagua/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine(" ");
+		Console.WriteLine("use 1 para sim e 0 para nao\n");
+		Console.WriteLine("A BOIA A ESTA LIGADA?");
+		double boia1 = Convert.ToDouble(Console.ReadLine());
+		
+		Console.WriteLine("A BOIA B ESTA LIGADA?");
+		double boia2 = Convert.ToDouble(Console.ReadLine());
+		
+		Console.WriteLine("A BOIA C ESTA LIGADA?");
+		double boia3 = Convert.ToDouble(Console.ReadLine());
+
+		
+		if (boia1 == 0 && boia2 == 0 && boia3 == 0)
+		{
+			Console.WriteLine("Ligue a válvula");
+		}
+	 	else if (boia1 == 1 && boia2 == 0 && boia3 == 0)
+		{
+			Console.WriteLine("Ligue a válvula");
+		}
+		else if (boia1 == 0 && boia2 > 0 )
+		{
+			Console.WriteLine("Erro");
+		}
+		else if (boia1 == 1 && boia2 == 1 && boia3 == 0)
+		{
+			Console.WriteLine("Ligue a bomba");
+			Console.WriteLine("desligue a valvula");
+		
+		}
+		else if (boia1 == 1 && boia2 == 1 && boia3 == 1)
+		{
+			
+			Console.WriteLine("desligue a bomba");
+			Console.WriteLine("A caixa d'agua esta cheia");
+		}
+	
+		
+		else
+		{
+			Console.WriteLine("Condição inválida.");
+		}
